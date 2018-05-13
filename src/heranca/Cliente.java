fa19705c04dccb2d41c1bcd60802aaafd95e65a7
robsoncampelo <robson.campelo@ifmg.edu.br>
2018-05-12 22:30:03 -0300

    Implementa atributos na classe Cliente

--- src/heranca/Cliente.java
+++ src/heranca/Cliente.java
@@ -1,5 +1,22 @@
 package heranca;
 
 public class Cliente {
-
+	
+	private String codigo;
+	private String nome;
+	
+	//MÉTODOS GETTERS AND SETTERS
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
