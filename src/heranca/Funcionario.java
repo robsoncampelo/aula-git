f79bc36c1aeb4e503ca41f41804bba3bc4436f5c
robsoncampelo <robson.campelo@ifmg.edu.br>
2018-05-13 11:24:37 -0300

    Teste de conflito (master)

--- src/heranca/Funcionario.java
+++ src/heranca/Funcionario.java
@@ -1,5 +1,23 @@
 package heranca;
 
 public class Funcionario {
+	
+	private String nomeFuncionario;
+	private String endereco;
+	
+	//MÉTODOS GETTERS AND SETTERS
+	
+	public String getNomeFuncionario() {
+		return nomeFuncionario;
+	}
+	public void setNomeFuncionario(String nomeFuncionario) {
+		this.nomeFuncionario = nomeFuncionario;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}	
 
 }
