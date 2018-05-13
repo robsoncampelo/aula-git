9fdad93aa2e1ad2c1f283a0532ac3e77a16f8969
robsoncampelo <robson.campelo@ifmg.edu.br>
2018-05-13 11:17:35 -0300

    Teste de conflito (versao-dados-funcionario)

--- src/heranca/Funcionario.java
+++ src/heranca/Funcionario.java
@@ -1,5 +1,23 @@
 package heranca;
 
 public class Funcionario {
+	
+	private String nome;
+	private String end;
+	
+	//MÉTODOS GETTERS AND SETTERS
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
 
 }
